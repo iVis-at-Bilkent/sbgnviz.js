0414a68ec7b703e0d1aab3467739c3c4f717172b
istemi-bahceci <istemi.bahceci@gmail.com>
2015-07-06 17:44:29 +0300

    Lets refactor EditorActionsManager in this way.

--- sample-app/sampleapp-components/js/EditorActionsManager.js
+++ sample-app/sampleapp-components/js/EditorActionsManager.js
@@ -23,68 +23,69 @@ function removeNode(nodeToBeDeleted)
   * params: additional parameters for this command
  */
 var Command = function (_do, undo, params) {
-    this._do = _do;
-    this.undo = undo;
-    this.params = params;
-}
+	this._do = _do;
+	this.undo = undo;
+	this.params = params;
+};
  
 var AddNodeCommand = function (newNode) 
 {
-    return new Command(addNode, removeNode, newNode);
+	return new Command(addNode, removeNode, newNode);
 };
 
 var RemoveNodeCommand = function (nodeTobeDeleted) 
 {
-    return new Command(removeNode, addNode, nodeTobeDeleted);
+	return new Command(removeNode, addNode, nodeTobeDeleted);
 };
 
  /**
  *  Description: A simple action manager that acts also as a undo-redo manager regarding Command Design Pattern
  *	Author: Istemi Bahceci<istemi.bahceci@gmail.com>
  */
-var editorActionsManager = {
-    undoStack: [],
-    redoStack: [],
+function EditorActionsManager()
+{
+	this.undoStack = [];
+	this.redoStack = [];
  
 	 /*
 	 *  Executes given command by calling do method of given command
 	 *  pushes the action to the undoStack after execution.
 	 */
-    _do: function (command) 
-    {
-        command._do(command.params);
-     	undoStack.push(command);
-    },
+	this._do = function (command) 
+	{
+		command._do(command.params);
+		this.undoStack.push(command);
+	};
 
 	 /*
 	 *  Undo last command.
 	 *  Pushes the reversed action to the redoStack after undo operation.
 	 */
-    undo: function () 
-    {
-    	var lastCommand = undoStack.pop();
-    	lastCommand.undo(lastCommand.params);
-    	redoStack.push(lastCommand);
-    },
+	this.undo = function () 
+	{
+		var lastCommand = undoStack.pop();
+		lastCommand.undo(lastCommand.params);
+		this.redoStack.push(lastCommand);
+	};
 
 	 /*
 	 *  Redo last command that is previously undid.
 	 *  This method basically calls do method for the last command that is popped of the redoStack.
 	 */
-    redo: function()
-    {
-    	var lastCommand = redoStack.pop();
-    	this._do(lastCommand)
-    },
+	this.redo = function()
+	{
+		var lastCommand = redoStack.pop();
+		this._do(lastCommand);
+	};
 
 	 /*
 	 *  Empties undo and redo stacks !
 	 */
-    reset: function()
-    {
-	    undoStack = [];
-	    redoStack =[];
-    }
+	this.reset = function()
+	{
+		this.undoStack = [];
+		this.redoStack =[];
+	};
 }
  
  
@@ -93,13 +94,13 @@ var editorActionsManager = {
  */
 function sampleRun() 
 {
-//    var editorActionsManager = new EditorActionsManager();
+	var editorActionsManager = new EditorActionsManager();
  
-    // issue commands
-    editorActionsManager._do(new AddNodeCommand(newNode));
+	// issue commands
+	editorActionsManager._do(new AddNodeCommand(newNode));
  
-    // undo redo mechanism
-    editorActionsManager.undo();
-    editorActionsManager.redo();
+	// undo redo mechanism
+	editorActionsManager.undo();
+	editorActionsManager.redo();
  
 }
