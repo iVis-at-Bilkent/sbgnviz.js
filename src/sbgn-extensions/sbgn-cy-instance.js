a099401b7029c6bad8519844f4b26b5e5a7b125d
metincansiper <metincansiper@gmail.com>
2017-04-18 15:29:04 +0300

    'padding-<pos>' is not supported by ctysocape.js use 'padding' instead of it
    Note that we are not able to set different paddings for 4 sides of nodes
    for now.

--- src/sbgn-extensions/sbgn-cy-instance.js
+++ src/sbgn-extensions/sbgn-cy-instance.js
@@ -107,17 +107,11 @@ module.exports = function () {
             'background-opacity': 0.5,
             'text-opacity': 1,
             'opacity': 1,
-            'padding-left': 0,
-            'padding-right': 0,
-            'padding-top': 0,
-            'padding-bottom': 0
+            'padding': 0
           })
           .selector("node:parent")
           .css({
-            'padding-left': getCompoundPaddings,
-            'padding-right': getCompoundPaddings,
-            'padding-top': getCompoundPaddings,
-            'padding-bottom': getCompoundPaddings
+            'padding': getCompoundPaddings
           })
           .selector("node[?clonemarker][class='perturbing agent']")
           .css({
@@ -169,8 +163,9 @@ module.exports = function () {
           })
           .selector("node:parent[class='compartment']")
           .css({
-            'padding-top': getCompoundPaddings() + options.extraCompartmentPadding,
-            'padding-bottom': getCompoundPaddings() + options.extraCompartmentPadding
+            'padding': function() {
+              return getCompoundPaddings() + options.extraCompartmentPadding;
+            }
           })
           .selector("node[bbox][class][class!='complex'][class!='compartment'][class!='submap']")
           .css({
