a823210f7b6a865b032b02da2cee428a17174003
Metin Can Siper <siperm@rjhb719.ohsu.edu>
2017-10-26 11:16:37 -0700

    In keyboardUtilities keep listening to keyboard events on $(document)

--- src/utilities/keyboard-input-utilities-factory.js
+++ src/utilities/keyboard-input-utilities-factory.js
@@ -6,28 +6,23 @@ var libUtilities = require('./lib-utilities');
 var libs = libUtilities.getLibs();
 var jQuery = $ = libs.jQuery;
 
-module.exports = function (param) {
-  var optionUtilities;
-  var options;
+module.exports = function () {
 
   function keyboardInputUtilities (param) {
-    optionUtilities = param.optionUtilities;
-    options = optionUtilities.getOptions();
 
     // Listen to events on network container
     $(document).ready(function () {
-      var networkContainer = $(options.networkContainerSelector);
-      networkContainer.on('keydown', '.integer-input', function(e){
+      $(document).on('keydown', '.integer-input', function(e){
         var value = $(this).attr('value');
         return keyboardInputUtilities.isIntegerFieldInput(value, e);
       });
 
-      networkContainer.on('keydown', '.float-input', function(e){
+      $(document).on('keydown', '.float-input', function(e){
         var value = $(this).attr('value');
         return keyboardInputUtilities.isFloatFieldInput(value, e);
       });
 
-      networkContainer.on('change', '.integer-input,.float-input', function(e){
+      $(document).on('change', '.integer-input,.float-input', function(e){
         var min   = $(this).attr('min');
         var max   = $(this).attr('max');
         var value = parseFloat($(this).val());
@@ -64,48 +59,48 @@ module.exports = function (param) {
     });
   }
 
-  keyboardInputUtilities.isNumberKey = function(e) {
+  $(document).isNumberKey = function(e) {
     return ( e.keyCode >= 48 && e.keyCode <= 57 ) || ( e.keyCode >= 96 && e.keyCode <= 105 );
   };
 
-  keyboardInputUtilities.isDotKey = function(e) {
+  $(document).isDotKey = function(e) {
     return e.keyCode === 190;
   };
 
-  keyboardInputUtilities.isMinusSignKey = function(e) {
+  $(document).isMinusSignKey = function(e) {
     return e.keyCode === 109 || e.keyCode === 189;
   };
 
-  keyboardInputUtilities.isLeftKey = function(e) {
+  $(document).isLeftKey = function(e) {
     return e.keyCode === 37;
   };
 
-  keyboardInputUtilities.isRightKey = function(e) {
+  $(document).isRightKey = function(e) {
     return e.keyCode === 39;
   };
 
-  keyboardInputUtilities.isBackspaceKey = function(e) {
+  $(document).isBackspaceKey = function(e) {
     return e.keyCode === 8;
   };
 
-  keyboardInputUtilities.isTabKey = function(e) {
+  $(document).isTabKey = function(e) {
     return e.keyCode === 9;
   };
 
-  keyboardInputUtilities.isEnterKey = function(e) {
+  $(document).isEnterKey = function(e) {
     return e.keyCode === 13;
   };
 
-  keyboardInputUtilities.isIntegerFieldInput = function(value, e) {
+  $(document).isIntegerFieldInput = function(value, e) {
     return this.isCtrlOrCommandPressed(e) || this.isMinusSignKey(e) || this.isNumberKey(e)
             || this.isBackspaceKey(e) || this.isTabKey(e) || this.isLeftKey(e) || this.isRightKey(e) || this.isEnterKey(e);
   };
 
-  keyboardInputUtilities.isFloatFieldInput = function(value, e) {
+  $(document).isFloatFieldInput = function(value, e) {
     return this.isIntegerFieldInput(value, e) || this.isDotKey(e);
   };
 
-  keyboardInputUtilities.isCtrlOrCommandPressed = function(e) {
+  $(document).isCtrlOrCommandPressed = function(e) {
     return e.ctrlKey || e.metaKey;
   };
 
