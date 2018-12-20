a5e156a4662923ccc7029b30433b3fe748d85e63
metincansiper <metincansiper@gmail.com>
2018-12-20 13:53:57 -0800

    In nwt-to-json fixed omitting shapeName style property problem

--- src/utilities/nwt-to-json-converter-factory.js
+++ src/utilities/nwt-to-json-converter-factory.js
@@ -51,6 +51,68 @@ module.exports = function() {
     } );
   }
 
+  function getElMap( graphData ) {
+    var map = {
+      nodes: {},
+      edges: {},
+      infoboxes: {}
+    };
+
+    graphData.edges.forEach( function( edge ) {
+      map.edges[ edge.data.id ] = edge;
+    } );
+
+    graphData.nodes.forEach( function( node ) {
+      map.nodes[ node.data.id ] = node;
+
+      var infoboxes = node.data.statesandinfos;
+
+      infoboxes.forEach( function( infobox ) {
+        map.infoboxes[ infobox.id ] = infobox;
+      } );
+    } );
+
+    return map;
+  }
+
+  function applyExtraStylesData( graphData, xmlObject ) {
+    var listOfStyles = xmlObject.querySelector('listOfStyles')
+    var styles = listOfStyles && listOfStyles.querySelectorAll('style');
+
+    if (!styles || styles.length === 0) {
+      return;
+    }
+
+    var attrToProp = {
+      'shapeName': 'shape-name'
+    };
+    var elMap = getElMap( graphData );
+
+    styles.forEach( function( style ) {
+      var idList = style.getAttribute('idList').split(' ');
+
+      Object.keys( attrToProp ).forEach( function( attrName ) {
+        var g = style.querySelector('g');
+        var val = g.getAttribute( attrName );
+
+        if ( val ) {
+          var propName = attrToProp[ attrName ];
+
+          idList.forEach( function( id ) {
+            if ( elMap.nodes[ id ] || elMap.edges[ id ] ) {
+              var el = elMap.nodes[ id ] || elMap.edges[ id ];
+              el.data[ propName ] = val;
+            }
+            else if ( elMap.infoboxes[ id ] ) {
+              var el = elMap.infoboxes[ id ];
+              el.style[ propName ] = val;
+            }
+          } );
+        }
+      } );
+    } );
+  }
+
   nwtToJson.convert = function(xmlObject) {
     var graphData = sbgnmlToJson.convert(xmlObject);
     var mapType = elementUtilities.mapType;
@@ -59,6 +121,8 @@ module.exports = function() {
       elementUtilities.fileFormat = 'nwt';
       extendEdgesData( graphData.edges, elementUtilities.isSIFEdge, sifEdgePropHandlerMap, xmlObject );
     }
+
+    applyExtraStylesData( graphData, xmlObject );
 
     return graphData;
   };
