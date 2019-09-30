2a2a5ef98eb8a578f3b04820dbb6d1d32b0aef0b
Hasan Balcı <balcihasan99@gmail.com>
2019-10-01 00:21:51 +0300

    Fix iVis-at-Bilkent/newt#476

--- src/utilities/element-utilities-factory.js
+++ src/utilities/element-utilities-factory.js
@@ -440,8 +440,8 @@ module.exports = function () {
     'BA simple chemical', 'BA macromolecule', 'BA nucleic acid feature',
     'BA perturbing agent', 'BA complex'];
   elementUtilities.epnTypes = ['macromolecule', 'nucleic acid feature', 'simple chemical',
-    'source and sink', 'unspecified entity',
-    'perturbing agent', 'complex'];
+    'source and sink', 'unspecified entity', 'perturbing agent', 'complex', 
+    'nucleic acid feature multimer', 'macromolecule multimer', 'simple chemical multimer', 'complex multimer'];
   elementUtilities.sifTypes = ['SIF macromolecule', 'SIF simple chemical'];
   elementUtilities.otherNodeTypes = ['compartment', 'tag', 'submap', 'topology group'];
 
