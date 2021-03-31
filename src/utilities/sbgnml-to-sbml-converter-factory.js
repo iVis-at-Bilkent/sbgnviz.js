897ad130d1f3ea65312869afbefeeabfa28903d6
Piotr Gawron <piotr.gawron@uni.lu>
2021-03-31 11:22:54 +0200

    use modified API

--- src/utilities/sbgnml-to-sbml-converter-factory.js
+++ src/utilities/sbgnml-to-sbml-converter-factory.js
@@ -1,54 +1,40 @@
-module.exports = function() {
+module.exports = function () {
 
     var jsonToSbgnml, elementUtilities, cy;
-  
-    function sbgnmlToSbml(param) {
-      jsonToSbgnml = param.jsonToSbgnmlConverter;
-      elementUtilities = param.elementUtilities;
-      cy = param.sbgnCyInstance.getCy();
-    }
-  
- 
-    sbgnmlToSbml.convert = function (xml, callback) {
-        var login = "login=anonymous&password=";
-        var url1 = "https://minerva-dev.lcsb.uni.lu/minerva/api/doLogin";
-        var url2 = "https://minerva-dev.lcsb.uni.lu/minerva/api/convert/SBGN-ML:SBML";
-        var myToken = "MINERVA_AUTH_TOKEN=";
-    
-        $.ajax({
-            type: 'post',
-            url: "/utilities/ServerRequest",
-            data: { address: url1, param: JSON.stringify(login), postType: "auth" },
-            success: function (data) {
-                taken = data.response.headers["set-cookie"][1];
-                var cookieArray = taken.split(';');
-                myToken += cookieArray[0].split('=')[1];
 
-                $.ajax({
-                type: 'post',
-                url: "/utilities/ServerRequest",
-                headers: {"Content-Type": "application/x-www-form-urlencoded; charset=UTF-8"},
-                data: { url: url2, file: xml, token: myToken, postType: "sendData" },
-                success: function (data) {                   
-                    callback({result: true, message: data.response , error :""});                 
-                },
-                error: function (error) {
-                  callback({result: false, error: error, message : ""});
-            
+    function sbgnmlToSbml(param) {
+        jsonToSbgnml = param.jsonToSbgnmlConverter;
+        elementUtilities = param.elementUtilities;
+        cy = param.sbgnCyInstance.getCy();
+    }
+
+
+    sbgnmlToSbml.convert = function (xml, callback) {
+        var conversionApiUrl = "https://minerva-dev.lcsb.uni.lu/minerva/api/convert/SBGN-ML:SBML";
+
+        return $.ajax({
+            type: 'post',
+            url: conversionApiUrl,
+            contentType: "text/xml; charset=UTF-8",
+            data: xml,
+            dataType: "text",
+            success: function (data, textStatus, xhr) {
+                if (xhr.status !== 200) {
+                    callback({result: false, error: data, message: "Invalid response code: " + xhr.status});
+                } else {
+                    callback({result: true, message: data, error: ""});
                 }
-            })
             },
             error: function (error) {
-                callback({result: false, error: error, message : ""});   
-        
-            }
-
-        });    
-       
-        
+                callback({result: false, error: error, message: ""});
+            },
+            fail: function (error) {
+                callback({result: false, error: error, message: ""});
+            },
+        })
     }
 
     return sbgnmlToSbml;
-  
-  }
+
+}
   
