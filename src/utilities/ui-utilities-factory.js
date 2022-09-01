f8c047e10a29574ab82323d5eba9faea65d7d39c
Selbi Ereshova <selbi.ereshova@ug.bilkent.edu.tr>
2022-09-01 14:07:44 +0300

    Updated spinner like in main branch

--- src/utilities/ui-utilities-factory.js
+++ src/utilities/ui-utilities-factory.js
@@ -22,20 +22,28 @@ module.exports = function () {
    }
 
    if ($('.' + className).length === 0) {
-     var containerWidth = $(options.networkContainerSelector).width();
-     var containerHeight = $(options.networkContainerSelector).height();
-     $(options.networkContainerSelector + ':parent').prepend('<i style="position: absolute; z-index: 9999999; left: ' + containerWidth / 2 + 'px; top: ' + containerHeight / 2 + 'px;" class="fa fa-spinner fa-spin fa-3x fa-fw ' + className + '"></i>');
+    var containerWidth = $(options.networkContainerSelector).width();
+    var containerHeight = $(options.networkContainerSelector).height();
+    $(options.networkContainerSelector + ':parent').prepend('<div style="position: absolute; z-index: 9999999; left: 45%; top: ' + containerHeight / 2 + 'px;" class="'+className+'-wrapper">'+
+    '<div style="margin: 0 auto; margin-bottom: 5px; width:50px"><i class="fa fa-spinner fa-spin fa-3x fa-fw ' + className + '"></i></div>'+
+    '<div style="width: 220px; height 20%"><i class="' + className + '-text">Take a sip of your coffee while you wait...</i></div>'+
+    '</div>');
+    $("."+className+'-text').fadeOut(0)
    }
  };
+ uiUtilities.showSpinnerText = function(className)
+ {
+   $("."+className+'-text').fadeIn(0)
+ }
 
  uiUtilities.endSpinner = function (className) {
    if (!className) {
      className = 'default-class';
    }
 
-   if ($('.' + className).length > 0) {
-     $('.' + className).remove();
-   }
+   if ($('.' + className + '-wrapper').length > 0) {
+    $('.' + className + '-wrapper').remove();
+  }
  };
 
  return uiUtilities;
