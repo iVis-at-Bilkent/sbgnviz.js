c89837327046803fc077788b0c7ae2c58842673c
Selbi Ereshova <selbi.ereshova@ug.bilkent.edu.tr>
2023-04-01 12:40:51 +0300

    Removed class for reaction for now

--- src/utilities/sbml-to-json-converter-factory.js
+++ src/utilities/sbml-to-json-converter-factory.js
@@ -200,7 +200,7 @@ module.exports = function () {
       // traverse reaction glyphs
       for(let i = 0; i < layout.getNumReactionGlyphs(); i++){
         let reactionGlyph = layout.getReactionGlyph(i);
-        let data = {id: reactionGlyph.getReactionId(), label: reactionMap.get(reactionGlyph.getReactionId())[0], class: sboToNodeClass[reactionMap.get(reactionGlyph.getReactionId())[1]], sboTerm: reactionMap.get(reactionGlyph.getReactionId())[1],
+        let data = {id: reactionGlyph.getReactionId(), label: reactionMap.get(reactionGlyph.getReactionId())[0], sboTerm: reactionMap.get(reactionGlyph.getReactionId())[1],
           width: 15, height: 15, bbox: {x: 0, y: 0, w: bbox.width, h: bbox.height}, statesandinfos: [] };
         let position = {x: reactionGlyph.getCurve().getCurveSegment(0).getStart().x() + 10, y: reactionGlyph.getCurve().getCurveSegment(0).getStart().y() + 10};
         reactionNodeMap.set(reactionGlyph.getReactionId(), {"data": data, "position": position, "group": "nodes", "classes": "reaction"});
